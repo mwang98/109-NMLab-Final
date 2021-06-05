7219ca6baf1925e03c27e29e4b710160d00a962c
mwang98 <35629082+MikeWangtc@users.noreply.github.com>
2021-06-05 14:59:14 +0800

    define router

--- 3_TodoApp/crypto-email/src/App.js
+++ 3_TodoApp/crypto-email/src/App.js
@@ -1,13 +1,39 @@
+import React, { Component } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { NavLink, Switch, Route } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+
 import logo from './logo.svg';
 import './App.css';
 import InboxPage from './containers/InboxPage';
+import NavigationBar from './containers/NavigationBar';
+
+const useStyles = makeStyles({
+    link: {
+        color: 'white',
+        textDecoration: 'none'
+    },
+    routeBtn: {
+        borderColor: 'white'
+    }
+})
 
 function App() {
-  return (
-    <div className="App">
-      <InboxPage />
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <div className="App">
+            <NavigationBar>
+                <Button variant="outlined" className={classes.routeBtn}><NavLink to="/inbox" className={classes.link}>Inbox</NavLink></Button>
+                <Button variant="outlined" className={classes.routeBtn}><NavLink to="/outbox" className={classes.link}>Outbox</NavLink></Button>
+                <Button variant="outlined" className={classes.routeBtn}><NavLink to="/draft" className={classes.link}>Draft</NavLink></Button>
+            </NavigationBar>
+
+
+            <Switch>
+                <Route path='/inbox' component={InboxPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
