d8362c28cfbd59e2b292d93cbae5896c247b84b7
mwang98 <35629082+MikeWangtc@users.noreply.github.com>
2021-06-06 15:30:05 +0800

    test ipfs server/client but faield

---
+++ 3_TodoApp/ipfs/client.js
@@ -0,0 +1,19 @@
+//using the infura.io node, otherwise ipfs requires you to run a //daemon on your own computer/server.
+const { create, urlSource } = require("ipfs-http-client");
+const ipfs = create("http://0.0.0.0:5002");
+
+async function getHash(str) {
+    const buffer = await Buffer.from(str, "ascii");
+    const result = await ipfs.add(buffer);
+
+    console.log(ipfs.getEndpointConfig());
+    console.log(await ipfs.version());
+    console.log(buffer);
+    console.log(buffer.toString());
+    console.log(result);
+
+    const file = await ipfs.add(urlSource("https://ipfs.io/images/ipfs-logo.svg"));
+    console.log(file);
+}
+
+getHash("Hello world");
