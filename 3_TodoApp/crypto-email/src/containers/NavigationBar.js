831fe1405e8ea6280b79246f6c8e268ebb4f1191
mwang98 <35629082+MikeWangtc@users.noreply.github.com>
2021-06-10 16:38:10 +0800

    finish user profile page and navigation

--- 3_TodoApp/crypto-email/src/containers/NavigationBar.js
+++ 3_TodoApp/crypto-email/src/containers/NavigationBar.js
@@ -1,78 +1,86 @@
-import React, { Component } from 'react';
-import { fade, withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Avatar from '@material-ui/core/Avatar';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Avatar from "@material-ui/core/Avatar";
 
 const styles = (theme) => ({
     grow: {
         flexGrow: 1,
     },
     title: {
-        display: 'none',
+        display: "none",
         margin: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
-        display: 'block',
+        [theme.breakpoints.up("sm")]: {
+            display: "block",
         },
     },
     sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-        display: 'flex',
+        display: "none",
+        [theme.breakpoints.up("md")]: {
+            display: "flex",
         },
     },
     routeBtn: {
-        '& > *': {
-          margin: theme.spacing(2),
+        "& > *": {
+            margin: theme.spacing(2),
         },
     },
     newMailBtn: {
         margin: theme.spacing(2),
-        color: 'white',
-        borderColor: 'white'
-    }
+        color: "white",
+        borderColor: "white",
+    },
 });
 
-
-class NavigationBar extends Component{
-    constructor(props){
+class NavigationBar extends Component {
+    constructor(props) {
         super(props);
     }
-    
-    handleProfileMenuOpen = (event) => {
-        this.setState({anchorEl: event.currentTarget});
-    }
 
-    render(){
-        const { children, classes } = this.props
-        return(
+    handleProfileMenuOpen = (event) => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    render() {
+        const { children, classes } = this.props;
+        return (
             <div className={classes.grow}>
-                <AppBar position="static">                    
+                <AppBar position="static">
                     <Toolbar>
-                        <Avatar alt='logo' src='/logo.png'/>
-                        <Typography className={classes.title} variant="h5" noWrap> Crypto Mail </Typography>
+                        <Avatar alt="logo" src="/logo.png" />
+                        <Typography className={classes.title} variant="h5" noWrap>
+                            {" "}
+                            Crypto Mail{" "}
+                        </Typography>
                         <div className={classes.routeBtn}>{children}</div>
                         <div className={classes.grow} />
-                        <Button className={classes.newMailBtn} variant="outlined">new mail</Button>
+                        <Button className={classes.newMailBtn} variant="outlined">
+                            new mail
+                        </Button>
                         <div className={classes.sectionDesktop}>
                             <IconButton
                                 edge="end"
                                 aria-label="account of current user"
                                 aria-haspopup="true"
                                 onClick={this.handleProfileMenuOpen}
-                                color="inherit">
-                            <AccountCircle />
+                                color="inherit"
+                            >
+                                <NavLink to="/profile">
+                                    <AccountCircle />
+                                </NavLink>
                             </IconButton>
                         </div>
                     </Toolbar>
                 </AppBar>
             </div>
-        )
+        );
     }
 }
 
-export default withStyles(styles)(NavigationBar)
+export default withStyles(styles)(NavigationBar);
---
+++ 3_TodoApp/crypto-email/src/containers/ProfilePage.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+import mockUsers from "../mock/user.json";
+
+const styles = (theme) => ({
+    container: {
+        justifyContent: "center",
+        padding: theme.spacing(5),
+    },
+    large: {
+        width: theme.spacing(30),
+        height: theme.spacing(30),
+    },
+});
+
+class CertifiedUserPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: null,
+            address: null,
+            description: "",
+            isCertified: false,
+        };
+    }
+
+    componentDidMount = async () => {
+        const { accounts } = this.props;
+
+        if (!accounts) return;
+        const address = accounts[0];
+
+        // get user profile from eth
+
+        // mock data
+        const user = mockUsers[0];
+        this.setState({
+            name: user.name,
+            address: address,
+            description: user.description,
+            isCertified: user.isCertified,
+        });
+    };
+
+    onChangeName = (event) => {
+        this.setState({ name: event.target.value });
+    };
+
+    onChangeDescription = (event) => {
+        this.setState({ description: event.target.value });
+    };
+
+    onSubmit = (event) => {
+        // set user profile to eth
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { name, address, description } = this.state;
+
+        return (
+            <div>
+                <Grid container className={classes.container}>
+                    <Grid container xs={4} className={classes.container}>
+                        <Avatar alt={name} src="/static/images/avatar/1.jpg" className={classes.large} />
+                        <TextField
+                            margin="dense"
+                            label="Name"
+                            variant="outlined"
+                            autoFocus
+                            fullWidth
+                            value={name}
+                            onChange={this.onChangeName}
+                        />
+                        <TextField
+                            margin="dense"
+                            label="Address"
+                            variant="outlined"
+                            InputProps={{ readOnly: true }}
+                            autoFocus
+                            fullWidth
+                            value={address}
+                        />
+                        <TextField
+                            margin="dense"
+                            label="Description"
+                            rowsMax={20}
+                            rows={10}
+                            variant="outlined"
+                            autoFocus
+                            fullWidth
+                            multiline
+                            value={description}
+                            onChange={this.onChangeDescription}
+                        />
+                        <Button variant="outlined" color="primary" onClick={this.onSubmit}>
+                            submit
+                        </Button>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
+}
+export default withStyles(styles)(CertifiedUserPage);
