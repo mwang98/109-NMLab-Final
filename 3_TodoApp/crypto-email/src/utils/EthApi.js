f9778d4e185572c5861b146f23eac331940c2656
mwang98 <35629082+MikeWangtc@users.noreply.github.com>
2021-06-10 18:39:02 +0800

    update

---
+++ 3_TodoApp/crypto-email/src/utils/EthApi.js
@@ -0,0 +1,142 @@
+import { getWeb3 } from "react.js-web3";
+
+import EmailSystemContract from "../build/contracts/EmailSystem.json";
+
+class EthApi {
+    constructor() {
+        this.web3 = null;
+        this.accounts = null;
+        this.contract = null;
+    }
+
+    setup = async () => {
+        try {
+            const web3 = await getWeb3();
+            const accounts = await web3.eth.getAccounts();
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = EmailSystemContract.networks[networkId];
+            console.log(networkId, deployedNetwork);
+            console.log(accounts);
+            const instance = new web3.eth.Contract(EmailSystemContract.abi, deployedNetwork && deployedNetwork.address);
+            this.web3 = web3;
+            this.accounts = accounts;
+            this.contract = instance;
+        } catch (error) {
+            alert(`Failed to load web3, accounts, or contract. Check console for details.`);
+            console.error(error);
+        }
+    };
+
+    multiMediaContentsToarray(_var) {
+        return [_var.fileName, _var.fileType, _var.IPFSHash];
+    }
+    sendMail = async (mail) => {
+        if (mail.senderAddr !== this.accounts[0]) {
+            console.log(`sender should be the account!!! ${this.accounts[0]}`);
+            return;
+        }
+        var mail_sol = [
+            mail.id,
+            mail.senderAddr,
+            mail.receiverAddr,
+            mail.subject,
+            mail.timestamp,
+            mail.contents,
+            mail.multiMediaContents.map(this.multiMediaContentsToarray),
+            mail.isOpen,
+        ];
+        console.log(mail_sol);
+        await this.contract.methods.sendMail(mail_sol).send({ from: mail.senderAddr });
+    };
+    deleteMail = async (userAddress, mail) => {
+        if (userAddress !== this.accounts[0]) {
+            console.log("userAddress should be the account!!!");
+            return;
+        }
+        var mail_sol = [
+            mail.id,
+            mail.senderAddr,
+            mail.receiverAddr,
+            mail.subject,
+            mail.timestamp,
+            mail.contents,
+            mail.multiMediaContents.map(this.multiMediaContentsToarray),
+            mail.isOpen,
+        ];
+        await this.contract.methods.deleteMail(userAddress, mail_sol).send({ from: userAddress });
+    };
+    saveMail = async (userAddress, mail) => {
+        if (userAddress !== this.accounts[0]) {
+            console.log("userAddress should be the account!!!");
+            return;
+        }
+        var mail_sol = [
+            mail.id,
+            mail.senderAddr,
+            mail.receiverAddr,
+            mail.subject,
+            mail.timestamp,
+            mail.contents,
+            mail.multiMediaContents.map(this.multiMediaContentsToarray),
+            mail.isOpen,
+        ];
+        await this.contract.methods.saveMail(userAddress, mail_sol).send({ from: userAddress });
+    };
+    openMail = async (mail) => {
+        if (mail.receiverAddr !== this.accounts[0]) {
+            console.log("receiver should be the account!!!");
+            return;
+        }
+        var mail_sol = [
+            mail.id,
+            mail.senderAddr,
+            mail.receiverAddr,
+            mail.subject,
+            mail.timestamp,
+            mail.contents,
+            mail.multiMediaContents.map(this.multiMediaContentsToarray),
+            mail.isOpen,
+        ];
+        await this.contract.methods.openMail(mail_sol).send({ from: mail.receiverAddr });
+    };
+    getUserName = async (userAddress) => {
+        if (userAddress !== this.accounts[0]) {
+            console.log("userAddress should be the account!!!");
+            return;
+        }
+        var result = await this.contract.methods.getUserName(userAddress).call();
+        console.log(result);
+    };
+    getInboxMails = async (userAddress) => {
+        if (userAddress !== this.accounts[0]) {
+            console.log("userAddress should be the account!!!");
+            return;
+        }
+        var result = await this.contract.methods.getInboxMails(userAddress).call();
+        console.log(result);
+    };
+    getOutboxMails = async (userAddress) => {
+        if (userAddress !== this.accounts[0]) {
+            console.log("userAddress should be the account!!!");
+            return;
+        }
+        var result = await this.contract.methods.getOutboxMails(userAddress).call();
+        console.log(result);
+    };
+    getDraftMails = async (userAddress) => {
+        if (userAddress !== this.accounts[0]) {
+            console.log("userAddress should be the account!!!");
+            return;
+        }
+        var result = await this.contract.methods.getDraftboxMails(userAddress).call();
+        console.log(result);
+    };
+}
+
+const getEthApi = async () => {
+    const api = new EthApi();
+    await api.setup();
+    return api;
+};
+
+export { getEthApi };
