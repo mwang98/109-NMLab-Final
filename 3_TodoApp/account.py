d92853dcaf200ea81fd6daf3b6bc62bf2ddf51aa
roberthuang0306 <b06901170@ntu.ed.tw>
2021-06-26 22:51:51 +0800

    update account.py

--- 3_TodoApp/account.py
+++ 3_TodoApp/account.py
@@ -4,7 +4,8 @@ import sys
 with open(sys.argv[1]) as f:
   data = json.load(f)['addresses'].items()
 
-for datum in data:
+for i, datum in enumerate(data):
+    print(f'({i})')
     print(f'address: {datum[0]}')
     print('public key:',''.join(list(map(lambda x: hex(x)[2:].zfill(2), datum[1]['publicKey']['data']))))
     print("private key:", "0x"+''.join(list(map(lambda x: hex(x)[2:].zfill(2), datum[1]['secretKey']['data']))), end='\n\n')
